8eeadf965acff48ceb5f0c96ea1e4c0771d46f52
am903103 <am903103@au.edu.tw>
2018-08-14 16:42:59 +0800

    Add Requirement in main

--- SwiftFactory/main.swift
+++ SwiftFactory/main.swift
@@ -6,3 +6,16 @@
 //  Copyright © 2018年 Gamma. All rights reserved.
 //
 
+//Step 1. Main
+
+print("Choose your starter Pokemon (B = Bulbasaur / C = Charmander / S = Squirtle)")
+
+let response = readLine(strippingNewline: true)
+
+let pokemon = PokemonFactory().getStarterPokemong(pokemonString: response!)
+
+if( pokemon != nil ){
+    print("You have picked \((pokemon?.name)!). It is a \((pokemon?.type)!)")
+}else {
+    print("Please pick a suitable Pokemon")
+}
