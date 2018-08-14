b903645c348b8d97e8c0209894cff959149660aa
am903103 <am903103@au.edu.tw>
2018-08-14 16:59:55 +0800

    Add concrete Pokemon

---
+++ SwiftFactory/Bulbasaur.swift
@@ -0,0 +1,14 @@
+//
+// Created by Apple on 2018/8/14.
+// Copyright (c) 2018 Gamma. All rights reserved.
+//
+
+//Step 3. Add Concrete Pokemon
+
+class Bulbasaur: Pokemon {
+    override init() {
+        super.init()
+        self.name = "Bulbasaur"
+        self.type = "Grass"
+    }
+}
---
+++ SwiftFactory/Charmander.swift
@@ -0,0 +1,14 @@
+//
+// Created by Apple on 2018/8/14.
+// Copyright (c) 2018 Gamma. All rights reserved.
+//
+
+//Step 3. Add Concrete Pokemon
+
+class Charmander: Pokemon {
+    override init() {
+        super.init()
+        self.name = "Charmander"
+        self.type = "Fire"
+    }
+}
